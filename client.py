763f552f5f76583b5480441bcb43b33759c8bc39
sahil <sru@andrew.cmu.edu>
2018-11-05 10:44:08 -0800

    Client code updated

--- client.py
+++ client.py
@@ -6,7 +6,7 @@ import sys, time, thread
 http_server = sys.argv[1]
 playerNum = sys.argv[2]
 # In seconds
-heartbeatInterval = sys.argv[3]
+heartbeatInterval = float(int(sys.argv[3]))
 conn = httplib.HTTPConnection(http_server)
 headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
 
@@ -27,22 +27,39 @@ def move():
 
 
 def heartbeat():
+	global conn
+	hbCount = 0
 	while 1:
-        params = urllib.urlencode({})
-        conn.request("GET", "/heartbeat?"+params)
-        rsp = conn.getresponse()
-        response = rsp.read()
-        print(response)
+		params = urllib.urlencode({})
+		try:
+			conn.request("GET", "/heartbeat?"+params)
+			rsp = conn.getresponse()
+			response = rsp.read()
+			print(response)
+		except:
+			print("Down")
+			if hbCount != 3:
+				hbCount += 1
 
-        # Sleep for the amount of time to simualte heartbeat
-        time.sleep(heartbeatInterval)
+		if hbCount == 3:
+			print("Actually down")
+			while 1:
+				try:
+					conn = httplib.HTTPConnection(http_server)
+					print(conn)
+					hbCount = 0
+					break
+				except:
+					continue
 
+		# Sleep for the amount of time to simualte heartbeat
+		time.sleep(heartbeatInterval)
 
 
 try:
 	print("In try")
 	thread.start_new_thread(move, ())
-    thread.start_new_thread(heartbeat, ())
+	thread.start_new_thread(heartbeat, ())
 except:
 	print("Error, unable to start new thread")
 while 1:
